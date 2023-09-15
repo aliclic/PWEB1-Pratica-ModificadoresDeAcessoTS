2aaae9aa77dfa8bd96b89451dccb9fbac864c183
Alic Victor <alicvictor11@gmail.com>
2023-09-15 14:00:35 -0300

    Update pessoa-juridica.ts

--- app/ts/model/pessoa-juridica.ts
+++ app/ts/model/pessoa-juridica.ts
@@ -1,17 +1,12 @@
 class PessoaJuridica extends Pessoa {
     
-    private cnpj: string;
+    readonly cnpj: string;
 
     constructor(nome: string, idade: number, dataNascimento: Date, cnpj: string) {
         super(nome + ' - Jurídica', idade, dataNascimento);
-        this._cnpj = cnpj; // não deve ser alterado dentro da própria classe
     }
 
     get _cnpj() {
         return this.cnpj;
-    }
-
-    set _cnpj(novoCnpj: string) {
-        this.cnpj = novoCnpj;
     }
 }
