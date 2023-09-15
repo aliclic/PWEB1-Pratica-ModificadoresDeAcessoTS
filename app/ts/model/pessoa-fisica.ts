b5a9532bc2b4d346499d6597ccbb9d0dcee7514e
Alic Victor <alicvictor11@gmail.com>
2023-09-15 14:00:12 -0300

    Update pessoa-fisica.ts

--- app/ts/model/pessoa-fisica.ts
+++ app/ts/model/pessoa-fisica.ts
@@ -1,18 +1,13 @@
 class PessoaFisica extends Pessoa {
 
-    private cpf: string;
+    readonly cpf: string;
 
     constructor(nome: string, idade: number, dataNascimento: Date, cpf: string) {
         super(nome + ' - Física', idade, dataNascimento);
-        this._cpf = cpf; // não deve ser alterado dentro da própria classe
     }
 
     get _cpf() {
         return this.cpf;
-    }
-
-    set _cpf(novoCpf: string) {
-        this.cpf = novoCpf;
     }
 
 }
